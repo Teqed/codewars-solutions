29c14d1119d1024a74d44ecd4b12b788585ad329
Timothy Quilling <teqed@shatteredsky.net>
2023-12-23 01:14:01 -0500

    Add 7-kyu - Highest and Lowest - Typescript

--- src/7-kyu/highest_and_lowest/typescript/main.ts
+++ src/7-kyu/highest_and_lowest/typescript/main.ts
@@ -1,4 +1,4 @@
-export const Kata = {
+export const Kata = { // eslint-disable-line @typescript-eslint/naming-convention
 	highAndLow(numbers: string): string {
 		const numbersArray = numbers.split(' ').map(Number);
 		return `${Math.max(...numbersArray)} ${Math.min(...numbersArray)}`;
